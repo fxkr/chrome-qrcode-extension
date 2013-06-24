4104a09cfa4f7e8f5ff8c626fbf089565c6b72d5
Felix Kaiser <felix.kaiser@fxkr.net>
2013-06-24 19:18:21 +0200

    Add code

---
+++ popup.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var qr_cellsize = 8;
+  var qr_margin = 2 * qr_cellsize;
+  var qr_levels = ["M", "L"]
+
+  var createImage = function(payload) {
+    for (var levelIndex in qr_levels) {
+      for (var typeNum = 1; typeNum <= 10; typeNum++) {
+        try {
+          var qr = qrcode(typeNum, qr_levels[levelIndex]);
+          qr.addData(payload);
+          qr.make();
+          return qr.createImgTag(qr_cellsize, qr_margin);
+        } catch(e) {
+          if (strStartsWith(e.message, "code length overflow")) {
+            // ignore and try to use bigger QR code format
+          } else {
+            throw e;
+          }
+        }
+      }
+    }
+  };
+
+  var updateImage = function() {
+    payload = document.getElementById('textbox').value;
+    document.getElementById('insert-qrcode-here').innerHTML =
+      createImage(payload) || "Error. URL too long?";
+  }
+
+  var strStartsWith = function(string, prefix) {
+    return !string.indexOf(prefix);
+  }
+
+  document.getElementById("close").onclick = function() {
+    window.close();
+  }
+
+  document.getElementById("textbox").onchange = function() {
+    updateImage();
+  }
+
+  chrome.tabs.getSelected(null, function(tab) {
+    document.getElementById('textbox').value = tab.url;
+    updateImage();
+  });
+});
